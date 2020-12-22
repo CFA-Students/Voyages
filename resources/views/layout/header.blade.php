f49d2c686f5a0dd1fa8758c3500a4573bafb51d4
E-jarod <ejilane.jarod@gmail.com>
2020-12-22 01:19:41 +0100

    header refait

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -1,52 +1,59 @@
 <header>
-    <div class="collapse bg-dark" id="navbarHeader">
-        <div class="container">
-            <div class="row">
-                <div class="col-sm-8 col-md-7 py-4">
-                    <h4 class="text-white">About</h4>
-                    <p class="text-white">Add some information about the album below, the author, or any other background context. Make it a few sentences long so folks can pick up some informative tidbits. Then, link them off to some social networking sites or contact information.</p>
-                </div>
-                <div class="col-sm-4 offset-md-1 py-4">
-                    <h4 class="text-white">Contact</h4>
-                    <ul class="list-unstyled">
-                        <li><a href="#" class="text-white">Follow on Twitter</a></li>
-                        <li><a href="#" class="text-white">Like on Facebook</a></li>
-                        <li><a href="#" class="text-white">Email me</a></li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="navbar navbar-dark bg-dark box-shadow">
-        <div class="container d-flex justify-content-between">
-            <a href="#" class="navbar-brand d-flex align-items-center">
-                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="mr-2"><path d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z"></path><circle cx="12" cy="13" r="4"></circle></svg>
-                <strong>Mon T-Shirt</strong>
-            </a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <ul class="navbar-nav">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href=""><i class="fas fa-plane-departure"></i>&nbsp;FNAC VOYAGES</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor02"
+                aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarColor02">
+            {{--            <ul class="navbar-nav mr-auto">--}}
+            {{--                <li class="nav-item active">--}}
+            {{--                    <a class="nav-link" href="">Features--}}
+            {{--                        <span class="sr-only">(current)</span>--}}
+            {{--                    </a>--}}
+            {{--                </li>--}}
+            {{--                <li class="nav-item dropdown">--}}
+            {{--                    <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="" role="button"--}}
+            {{--                       aria-haspopup="true" aria-expanded="false">Autres Pages</a>--}}
+            {{--                    <div class="dropdown-menu">--}}
+            {{--                        <a class="dropdown-item" href="">Action</a>--}}
+            {{--                        <a class="dropdown-item" href="">Another action</a>--}}
+            {{--                        <a class="dropdown-item" href="">Something else here</a>--}}
+            {{--                        <div class="dropdown-divider"></div>--}}
+            {{--                        <a class="dropdown-item" href="">Separated link</a>--}}
+            {{--                    </div>--}}
+            {{--                </li>--}}
+            {{--            </ul>--}}
+            <ul class="navbar-nav ml-auto">
                 <li class="nav-item">
-                    <a class="nav-link" href="#"><i class="fa fa-shopping-cart"></i> Panier</a>
+                    <form class="form-inline my-2 my-lg-0">
+                        <input id="search" class="form-control mr-sm-2" type="text"
+                               placeholder="Le voyage de vos rêves !">
+                        <label for="search">
+                            <button class="btn btn-secondary my-2 my-sm-0" type="submit">Rechercher</button>
+                        </label>
+                    </form>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href=""><i class="fa fa-shopping-cart"></i> Panier</a>
                 </li>
             </ul>
         </div>
-    </div>
+    </nav>
 
     <nav class="navbar navbar-expand-lg navbar-light bg-red">
-        <a class="navbar-brand" href="#"></a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <a class="navbar-brand"></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
             <ul class="navbar-nav">
                 @foreach($categories as $category)
-                <li class="nav-item">
-                    <a class="nav-link" href="#">{{$category->nom}}</a>
-                </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="">{{$category->nom}}</a>
+                    </li>
                 @endforeach
-
             </ul>
         </div>
     </nav>
